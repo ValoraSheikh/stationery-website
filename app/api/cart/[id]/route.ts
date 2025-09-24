ad7bfdfd1edd63b3094a975f621b58063e321860
Aman Sheikh <haarishsheikh04@gmail.com>
2025-09-24 21:59:06 +0530

    Create DELETE cart by id route

---
+++ app/api/cart/[id]/route.ts
@@ -0,0 +1,50 @@
+import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import mongoose from "mongoose";
+import dbConnect from "@/lib/connectDB";
+import Cart from "@/models/Cart.model";
+import User from "@/models/User.model";
+import { authOptions } from "../../auth/[...nextAuth]/options";
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { productId: string } }
+) {
+  try {
+    await dbConnect();
+    const session = await getServerSession(authOptions);
+
+    if (!session?.user?.email) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+
+    const user = await User.findOne({ email: session.user.email });
+    if (!user) {
+      return NextResponse.json({ message: "User not found" }, { status: 404 });
+    }
+
+    const userId = user._id;
+    const productId = new mongoose.Types.ObjectId(params.productId);
+
+    const cart = await Cart.findOneAndUpdate(
+      { userId },
+      { $pull: { items: { productId } } },
+      { new: true }
+    );
+
+    if (!cart) {
+      return NextResponse.json({ message: "Cart not found" }, { status: 404 });
+    }
+
+    return NextResponse.json(
+      { message: "Item removed", cart },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error removing item from cart:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
