7f075fdcc13a87c6cb9a36dcbd93dc85e0d8a767
Aman Sheikh <haarishsheikh04@gmail.com>
2025-09-25 16:35:27 +0530

    Created product pages

---
+++ app/(app)/products/[id]/page.tsx
@@ -0,0 +1,405 @@
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Tab,
+  TabGroup,
+  TabList,
+  TabPanel,
+  TabPanels,
+} from "@headlessui/react";
+import { StarIcon } from "@heroicons/react/20/solid";
+import { HeartIcon, MinusIcon, PlusIcon } from "@heroicons/react/24/outline";
+import Image from "next/image";
+
+const product = {
+  name: "Zip Tote Basket",
+  price: "$140",
+  rating: 4,
+  images: [
+    {
+      id: 1,
+      name: "Angled view",
+      src: "https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-03-product-01.jpg",
+      alt: "Angled front view with bag zipped and handles upright.",
+    },
+    {
+      id: 2,
+      name: "Front view",
+      src: "https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-03-product-02.jpg",
+      alt: "Front view with bag zipped and handles upright.",
+    },
+    {
+      id: 3,
+      name: "Back view",
+      src: "https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-03-product-03.jpg",
+      alt: "Back view with bag zipped and straps hanging down.",
+    },
+    {
+      id: 4,
+      name: "Back angle open view",
+      src: "https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-03-product-04.jpg",
+      alt: "Back angled view with bag open and handles to the side.",
+    },
+  ],
+  colors: [
+    {
+      id: "washed-black",
+      name: "Washed Black",
+      classes: "bg-gray-700 checked:outline-gray-700",
+    },
+    {
+      id: "white",
+      name: "White",
+      classes: "bg-white checked:outline-gray-400",
+    },
+    {
+      id: "washed-gray",
+      name: "Washed Gray",
+      classes: "bg-gray-500 checked:outline-gray-500",
+    },
+  ],
+  description: `
+    <p>The Zip Tote Basket is the perfect midpoint between shopping tote and comfy backpack. With convertible straps, you can hand carry, should sling, or backpack this convenient and spacious bag. The zip top and durable canvas construction keeps your goods protected for all-day use.</p>
+  `,
+  details: [
+    {
+      name: "Features",
+      items: [
+        "Multiple strap configurations",
+        "Spacious interior with top zip",
+        "Leather handle and tabs",
+        "Interior dividers",
+        "Stainless strap loops",
+        "Double stitched construction",
+        "Water-resistant",
+      ],
+    },
+    {
+      name: "Care",
+      items: [
+        "Spot clean as needed",
+        "Hand wash with mild soap",
+        "Machine wash interior dividers",
+        "Treat handle and tabs with leather conditioner",
+      ],
+    },
+    {
+      name: "Shipping",
+      items: [
+        "Free shipping on orders over $300",
+        "International shipping available",
+        "Expedited shipping options",
+        "Signature required upon delivery",
+      ],
+    },
+    {
+      name: "Returns",
+      items: [
+        "Easy return requests",
+        "Pre-paid shipping label included",
+        "10% restocking fee for returns",
+        "60 day return window",
+      ],
+    },
+  ],
+};
+
+const reviews = [
+  {
+    id: 1,
+    rating: 5,
+    content: `
+      <p>This icon pack is just what I need for my latest project. There's an icon for just about anything I could ever need. Love the playful look!</p>
+    `,
+    date: "July 16, 2021",
+    datetime: "2021-07-16",
+    author: "Emily Selman",
+    avatarSrc:
+      "https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80",
+  },
+  {
+    id: 2,
+    rating: 5,
+    content: `
+      <p>Blown away by how polished this icon pack is. Everything looks so consistent and each SVG is optimized out of the box so I can use it directly with confidence. It would take me several hours to create a single icon this good, so it's a steal at this price.</p>
+    `,
+    date: "July 12, 2021",
+    datetime: "2021-07-12",
+    author: "Hector Gibbons",
+    avatarSrc:
+      "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80",
+  },
+  {
+    id: 3,
+    rating: 4,
+    content: `
+      <p>Really happy with look and options of these icons. I've found uses for them everywhere in my recent projects. I hope there will be 20px versions in the future!</p>
+    `,
+    date: "July 6, 2021",
+    datetime: "2021-07-06",
+    author: "Mark Edwards",
+    avatarSrc:
+      "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixqx=oilqXxSqey&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },
+];
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function Example() {
+  return (
+    <>
+      <div className="bg-white">
+        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+          <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
+            {/* Image gallery */}
+            <TabGroup className="flex flex-col-reverse">
+              {/* Image selector */}
+              <div className="mx-auto mt-6 w-full max-w-2xl sm:block lg:max-w-none">
+                <TabList className="grid grid-cols-4 gap-6">
+                  {product.images.map((image) => (
+                    <Tab
+                      key={image.id}
+                      className="group relative flex h-24 cursor-pointer items-center justify-center rounded-md bg-white text-sm font-medium text-gray-900 uppercase hover:bg-gray-50 focus:ring-3 focus:ring-indigo-500/50 focus:ring-offset-4 focus:outline-hidden"
+                    >
+                      <span className="sr-only">{image.name}</span>
+                      <span className="absolute inset-0 overflow-hidden rounded-md">
+                        <Image
+                          height={500}
+                          width={500}
+                          alt=""
+                          src={image.src}
+                          className="size-full object-cover"
+                        />
+                      </span>
+                      <span
+                        aria-hidden="true"
+                        className="pointer-events-none absolute inset-0 rounded-md ring-2 ring-transparent ring-offset-2 group-data-selected:ring-indigo-500"
+                      />
+                    </Tab>
+                  ))}
+                </TabList>
+              </div>
+
+              <TabPanels>
+                {product.images.map((image) => (
+                  <TabPanel key={image.id}>
+                    <Image
+                      height={500}
+                      width={500}
+                      alt={image.alt}
+                      src={image.src}
+                      className="aspect-square w-full object-cover sm:rounded-lg"
+                    />
+                  </TabPanel>
+                ))}
+              </TabPanels>
+            </TabGroup>
+
+            {/* Product info */}
+            <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
+              <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+                {product.name}
+              </h1>
+
+              <div className="mt-3">
+                <h2 className="sr-only">Product information</h2>
+                <p className="text-3xl tracking-tight text-gray-900">
+                  {product.price}
+                </p>
+              </div>
+
+              {/* Reviews */}
+              <div className="mt-3">
+                <h3 className="sr-only">Reviews</h3>
+                <div className="flex items-center">
+                  <div className="flex items-center">
+                    {[0, 1, 2, 3, 4].map((rating) => (
+                      <StarIcon
+                        key={rating}
+                        aria-hidden="true"
+                        className={classNames(
+                          product.rating > rating
+                            ? "text-indigo-500"
+                            : "text-gray-300",
+                          "size-5 shrink-0"
+                        )}
+                      />
+                    ))}
+                  </div>
+                  <p className="sr-only">{product.rating} out of 5 stars</p>
+                </div>
+              </div>
+
+              <div className="mt-6">
+                <h3 className="sr-only">Description</h3>
+
+                <div
+                  dangerouslySetInnerHTML={{ __html: product.description }}
+                  className="space-y-6 text-base text-gray-700"
+                />
+              </div>
+
+              <form className="mt-6">
+                {/* Colors */}
+                <div>
+                  <h3 className="text-sm font-medium text-gray-600">Color</h3>
+
+                  <fieldset aria-label="Choose a color" className="mt-2">
+                    <div className="flex items-center gap-x-3">
+                      {product.colors.map((color) => (
+                        <div
+                          key={color.id}
+                          className="flex rounded-full outline -outline-offset-1 outline-black/10"
+                        >
+                          <input
+                            defaultValue={color.id}
+                            defaultChecked={color === product.colors[0]}
+                            name="color"
+                            type="radio"
+                            aria-label={color.name}
+                            className={classNames(
+                              color.classes,
+                              "size-8 appearance-none rounded-full forced-color-adjust-none checked:outline-2 checked:outline-offset-2 focus-visible:outline-3 focus-visible:outline-offset-3"
+                            )}
+                          />
+                        </div>
+                      ))}
+                    </div>
+                  </fieldset>
+                </div>
+
+                <div className="mt-10 flex">
+                  <button
+                    type="submit"
+                    className="flex max-w-xs flex-1 items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50 focus:outline-hidden sm:w-full"
+                  >
+                    Add to bag
+                  </button>
+
+                  <button
+                    type="button"
+                    className="ml-4 flex items-center justify-center rounded-md px-3 py-3 text-gray-400 hover:bg-gray-100 hover:text-gray-500"
+                  >
+                    <HeartIcon aria-hidden="true" className="size-6 shrink-0" />
+                    <span className="sr-only">Add to favorites</span>
+                  </button>
+                </div>
+              </form>
+
+              <section aria-labelledby="details-heading" className="mt-12">
+                <h2 id="details-heading" className="sr-only">
+                  Additional details
+                </h2>
+
+                <div className="divide-y divide-gray-200 border-t border-gray-200">
+                  {product.details.map((detail) => (
+                    <Disclosure key={detail.name} as="div">
+                      <h3>
+                        <DisclosureButton className="group relative flex w-full items-center justify-between py-6 text-left">
+                          <span className="text-sm font-medium text-gray-900 group-data-open:text-indigo-600">
+                            {detail.name}
+                          </span>
+                          <span className="ml-6 flex items-center">
+                            <PlusIcon
+                              aria-hidden="true"
+                              className="block size-6 text-gray-400 group-hover:text-gray-500 group-data-open:hidden"
+                            />
+                            <MinusIcon
+                              aria-hidden="true"
+                              className="hidden size-6 text-indigo-400 group-hover:text-indigo-500 group-data-open:block"
+                            />
+                          </span>
+                        </DisclosureButton>
+                      </h3>
+                      <DisclosurePanel className="pb-6">
+                        <ul
+                          role="list"
+                          className="list-disc space-y-1 pl-5 text-sm/6 text-gray-700 marker:text-gray-300"
+                        >
+                          {detail.items.map((item) => (
+                            <li key={item} className="pl-2">
+                              {item}
+                            </li>
+                          ))}
+                        </ul>
+                      </DisclosurePanel>
+                    </Disclosure>
+                  ))}
+                </div>
+              </section>
+            </div>
+          </div>
+
+
+
+      <div className="bg-white pt-20 lg:px-36">
+        <div>
+          <h2 className="sr-only">Customer Reviews</h2>
+
+          <div className="-my-10">
+            {reviews.map((review, reviewIdx) => (
+              <div
+                key={review.id}
+                className="flex space-x-4 text-sm text-gray-500"
+              >
+                <div className="flex-none py-10">
+                  <Image
+                    height={500}
+                    width={500}
+                    alt=""
+                    src={review.avatarSrc}
+                    className="size-10 rounded-full bg-gray-100"
+                  />
+                </div>
+                <div
+                  className={classNames(
+                    reviewIdx === 0 ? "" : "border-t border-gray-200",
+                    "flex-1 py-10"
+                  )}
+                >
+                  <h3 className="font-medium text-gray-900">{review.author}</h3>
+                  <p>
+                    <time dateTime={review.datetime}>{review.date}</time>
+                  </p>
+
+                  <div className="mt-4 flex items-center">
+                    {[0, 1, 2, 3, 4].map((rating) => (
+                      <StarIcon
+                        key={rating}
+                        aria-hidden="true"
+                        className={classNames(
+                          review.rating > rating
+                            ? "text-yellow-400"
+                            : "text-gray-300",
+                          "size-5 shrink-0"
+                        )}
+                      />
+                    ))}
+                  </div>
+                  <p className="sr-only">{review.rating} out of 5 stars</p>
+
+                  <div
+                    dangerouslySetInnerHTML={{ __html: review.content }}
+                    className="mt-4 text-sm/6 text-gray-500"
+                  />
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+
+
+
+          
+        </div>
+      </div>
+
+
+    </>
+  );
+}
---
+++ app/(app)/products/page.tsx
@@ -0,0 +1,12 @@
+import CategoryFilter from "@/components/Product/categoryFilter";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <CategoryFilter />
+    </>
+  );
+};
+
+export default page;
