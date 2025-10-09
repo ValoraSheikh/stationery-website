e4354510cfb7227dc6b19b3d76b12e3e8fdbab72
Aman Sheikh <haarishsheikh04@gmail.com>
2025-10-09 12:05:56 +0530

    Implemented fetch User API for admin routes

---
+++ app/api/admin/user/route.ts
@@ -0,0 +1,24 @@
+
+import dbConnect from "@/lib/connectDB";
+import User from "@/models/User.model";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+import { authOptions } from "../../auth/[...nextauth]/options";
+
+export async function GET() {
+  await dbConnect();
+
+  const session = await getServerSession(authOptions);
+  
+  if (!session?.user?.email) {
+    return NextResponse.json({ error: "Not authenticated" }, { status: 401 });
+  }
+
+  try {
+    const users = await User.find().sort({ createdAt: -1 }).select("-password");
+    return NextResponse.json(users);
+    
+  } catch (err) {
+    return NextResponse.json({ error: "Failed to fetch users" }, { status: 500 });
+  }
+}
