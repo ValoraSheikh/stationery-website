f73d3a5356bcdaa3c992f0f5744ba2d49967ec6c
Aman Sheikh <haarishsheikh04@gmail.com>
2025-10-09 12:07:43 +0530

    Make some changes for Installation

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,11 @@ const nextConfig: NextConfig = {
         hostname: 'plus.unsplash.com',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+        pathname: '/**',
+      },
     ],
   },
 };
